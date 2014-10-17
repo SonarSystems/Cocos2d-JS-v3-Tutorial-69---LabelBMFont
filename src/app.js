9911e04f3ff2a07df1a702de5bcf94073bc2a8b9
Frahaan Hussain <Sonar Systems>
2014-10-17 09:21:20 +0100

    LabelBMFont

---
+++ src/app.js
@@ -0,0 +1,35 @@
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        //this.addChild(sprite, 0);
+
+        var label = new cc.LabelBMFont("Hello World", res.LabelBMFont_png);
+        label.setPosition(cc.p(size.width / 2, size.height / 2));
+        label.color = cc.color.RED;
+        this.addChild(label);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
